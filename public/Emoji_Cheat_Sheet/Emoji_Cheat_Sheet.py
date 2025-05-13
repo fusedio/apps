d30e6f2d1293699f8befcd4c79f4d3bd4aff338d
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-05-13 15:29:50 -0700

    update Emoji_Cheat_Sheet for new streamlit version (#34)

--- public/Emoji_Cheat_Sheet/Emoji_Cheat_Sheet.py
+++ public/Emoji_Cheat_Sheet/Emoji_Cheat_Sheet.py
@@ -1,25 +1,32 @@
 import micropip
-await micropip.install('requests')
+
+await micropip.install("requests")
 import requests
 import pandas as pd
 import streamlit as st
-st.set_page_config(page_title = "Emoji Shortcodes", page_icon="🕹️", layout = "wide", initial_sidebar_state = "expanded")
+
+st.set_page_config(page_title="Emoji Shortcodes", page_icon="🕹️", layout="wide", initial_sidebar_state="expanded")
 st.markdown(
-    '''
+    """
     Direct Copy from [here](https://github.com/streamlit/emoji-shortcodes/blob/master/streamlit_app.py) by [Thiago Teixeira](https://github.com/tvst) 
-    ''')
-@st.cache_data(ttl=60*60*12)
+    """
+)
+
+
+@st.cache_data(ttl=60 * 60 * 12)
 def fetch_emojis():
-    resp = requests.get(
-        'https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json')
+    resp = requests.get("https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json")
     json = resp.json()
     codes, emojis = zip(*json.items())
-    return pd.DataFrame({
-        'Emojis': emojis,
-        'Shortcodes': [f':{code}:' for code in codes],
-    })
+    return pd.DataFrame(
+        {
+            "Emojis": emojis,
+            "Shortcodes": [f"`:{code}:`" for code in codes],
+        }
+    )
 
-'''
+
+"""
 # Streamlit emoji shortcodes
 
 Below are all the emoji shortcodes supported by Streamlit.
@@ -29,7 +36,7 @@ Shortcodes are a way to enter emojis using pure ASCII. So you can type this `:sm
 
 (Keep in mind you can also enter emojis directly as Unicode in your Python strings too — you don't
 *have to* use a shortcode)
-'''
+"""
 
 emojis = fetch_emojis()
 
